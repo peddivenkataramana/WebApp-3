d78b47654d079d916cf5fd360e160261fafea4d4
peddivenkataramana <peddivenkataramana78@gmail.com>
2022-11-30 01:21:58 -0600

    added login

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var passport = require('passport'); 
+var LocalStrategy = require('passport-local').Strategy;
 
 require('dotenv').config(); 
 const connectionString =  
@@ -12,6 +14,19 @@ mongoose.connect(connectionString,
 {useNewUrlParser: true, 
 useUnifiedTopology: true}); 
 
+// passport.use(new LocalStrategy( 
+//   function(username, password, done) { 
+//     Account.findOne({ username: username }, function (err, user) { 
+//       if (err) { return done(err); } 
+//       if (!user) { 
+//         return done(null, false, { message: 'Incorrect username.' }); 
+//       } 
+//       if (!user.validPassword(password)) { 
+//         return done(null, false, { message: 'Incorrect password.' }); 
+//       } 
+//       return done(null, user); 
+//     })})); 
+   
 //Get the default connection 
 var db = mongoose.connection; 
  
@@ -19,7 +34,6 @@ var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:')); 
 db.once("open", function(){ 
   console.log("Connection to DB succeeded")}); 
- 
 
 //Get the default connection 
 var Umbrella = require("./models/umbrella"); 
@@ -28,7 +42,7 @@ var usersRouter = require('./routes/users');
 var umbrellaRouter = require('./routes/umbrella');
 var gridbuildRouter = require('./routes/gridbuild');
 var selectorRouter = require('./routes/selector');
-var resourceRouter = require('./routes/resource')
+var resourceRouter = require('./routes/resource');
 
 var app = express();
 
@@ -40,7 +54,17 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(require('express-session')({ 
+  secret: 'keyboard cat', 
+  resave: false, 
+  saveUninitialized: false 
+})); 
+app.use(passport.initialize()); 
+app.use(passport.session()); 
+
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -48,6 +72,15 @@ app.use('/umbrella', umbrellaRouter);
 app.use('/gridbuild', gridbuildRouter);
 app.use('/selector', selectorRouter);
 app.use('/resource', resourceRouter);
+
+// passport config 
+// Use the existing connection 
+// The Account model  
+var Account =require('./models/account'); 
+ 
+passport.use(new LocalStrategy(Account.authenticate())); 
+passport.serializeUser(Account.serializeUser()); 
+passport.deserializeUser(Account.deserializeUser()); 
 
 // catch 404 and forward to error handler
 app.use(function (_req, _res, next) {
@@ -64,8 +97,6 @@ app.use(function (err, req, res, _next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
-module.exports = app;
 
  // We can seed the collection if needed on server start 
 async function recreateDB(){ 
@@ -99,5 +130,4 @@ Umbrella({color:"black",  cost:3, hieght:33});
 let reseed = true; 
 if (reseed) { recreateDB();} 
 
-
- 
+module.exports = app;
